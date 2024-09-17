6889f1b68956e283916b24f0a47dab254d2d10f4
TsimermanE <86789742+tsimer123@users.noreply.github.com>
2024-09-17 13:14:44 +0300

    added shedul param and parse mac (#2)

--- src/handler_commands.py
+++ src/handler_commands.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from config import list_command
 from handlers.handler_get_command import hand_result
 from logics.get_command import get_command
 from sql.model import TaskEquipmentHandlerModelGet
@@ -11,13 +12,7 @@ async def run_command(task_rb: TaskEquipmentHandlerModelGet):
         f'{datetime.now()}: start run_command for task {task_rb.task_id}, equipment {task_rb.serial_in_sourse}, command: {task_rb.type_task}'
     )
 
-    if (
-        task_rb.type_task == 'get_shedule'
-        or task_rb.type_task == 'get_leave_time'
-        or task_rb.type_task == 'get_tarif_mask'
-        or task_rb.type_task == 'set_leave_time'
-        or task_rb.type_task == 'set_tarif_mask'
-    ):
+    if task_rb.type_task in list_command:
         result = await get_command(task_rb)
         await hand_result(result)
         print(
